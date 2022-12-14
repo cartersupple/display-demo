83a191842e2a6d036630495e5f5b1347c83ad7a7
Carter Supple <Cartersupple123@gmail.com>
2022-12-13 22:48:51 -0800

    import css into app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import images from './api-mock.json';
+import './app.css'
 const App = () => {
   const [imageList, setImageList] = useState(images.resources);
   return (
