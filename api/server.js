186e75a600b2e2ece4f7ceb655edee53aaf79ece
Carter Supple <Cartersupple123@gmail.com>
2022-12-14 01:16:14 -0800

    got distracted but back on track

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const {json} = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+app.get('/photos', async (req, res)=> {
+return res.send({message :'yo'});
+});
+const PORT = 7000;
+
+app.listen(PORT, console.log('Its lit on port (7000)'));
